81f2e43505d74e313c9fbd3258f45ced4e75fd94
misopig8 <misopig8@gmail.com>
2024-11-17 15:27:27 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -14,11 +14,5 @@ pages ={
 
 st.sidebar.title("키오스크 메뉴")
 page = st.sidebar.selectbox("페이지 선택", ["고객용 페이지", "직원용 페이지"])
-
-#     if page == "고객용 페이지":
-#         customer_page()
-#     elif page == "직원용 페이지":
-#         staff_page()
-
-# if __name__ == "__main__":
-#     main()
+pg = st.navigation(pages)
+pg.run()
